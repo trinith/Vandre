88d09553482ab2b9e777556dd3150949bcd44c39
Gary <gtexmo@gmail.com>
2024-09-27 10:04:53 -0700

    Probably don't need it here but I discovered std::any and made a simple dependency container object.

---
+++ source/Vandre/DependencyContainer.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <any>
+#include <optional>
+#include <string>
+#include <unordered_map>
+
+struct DependencyContainer
+{
+	std::unordered_map<std::string, std::any> _components;
+
+	template <typename TType, typename... TArgs>
+	std::optional<std::reference_wrapper<TType>> AddComponent(TArgs&&... args)
+	{
+		std::any item = TType{ std::forward<TArgs>(args)... };
+		auto emplaceResult = _components.try_emplace(item.type().name(), std::move(item));
+		if (emplaceResult.second)
+		{
+			TType& emplacedItem = std::any_cast<TType&>(emplaceResult.first->second);
+			return emplacedItem;
+		}
+
+		return std::nullopt;
+	}
+
+	template <typename TType>
+	std::optional<std::reference_wrapper<TType>> GetComponent()
+	{
+		auto findResult = _components.find(typeid(TType).name());
+		if (findResult != _components.end())
+		{
+			TType& item = std::any_cast<TType&>(findResult->second);
+			return item;
+		}
+
+		return std::nullopt;
+	}
+};
